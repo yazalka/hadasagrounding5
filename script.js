e618c49037f7fb3d10530e44072a09c76e4a020d
Yazan Alkassir <yazan.kassir1@gmail.com>
2017-12-18 10:09:30 +0100

    grounding5

---
+++ script.js
@@ -0,0 +1,51 @@
+
+var scores = { 
+
+	"jake": 0,
+
+	"dave": 0,
+
+	"kate": 0,
+}
+
+
+
+function jakescore(){
+
+	
+
+	document.getElementById("Jake").innerHTML = scores.jake += 1;
+
+	
+
+}
+
+function katescore() {
+
+
+
+	document.getElementById("Kate").innerHTML = scores.kate += 1;
+
+
+}
+
+
+function davescore() {
+
+
+
+document.getElementById("Dave").innerHTML = scores.dave += 1;
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
